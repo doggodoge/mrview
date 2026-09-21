9a5239d61e042fb449e167ac9065d446afcae107
Gary Moore <gary@mooremoore.net>
2026-09-21 02:27:26 +0100

    finish simple implementation of app
    The GitHub repo this targets is hard-coded for now to ghostty. It's got
    a nice number of open PRs to test against. I'll be changing this to be a
    config driven number of PRs in the future.
    
    Really this is just something I want at work to quickly check open PRs
    for review. I had a TUI version of something like this before, but I
    wanted to learn libadwaita and GTK4, and figured I'd go with C as well
    to get a bit better at the lang.
    
    Happy with the result so far, and happy with my memory management
    strategy.

--- main.c
+++ main.c
@@ -1,60 +1,95 @@
 #include <adwaita.h>
 #include <stdio.h>
-#include <string.h>
 
 #include "base.h"
+#include "prs.h"
+#include "pull_request_card.h"
 #include "static_arena.h"
 #include "string_view.h"
-#include "prs.h"
 
 #define PR_STORAGE_CAPACITY (16 * 1024 * 1024)
+#define TEST_REPOSITORY "ghostty-org/ghostty"
 
 typedef struct {
-    char label_buf[1024];
-    i32 counter;
+	Static_Arena pr_arena;
+	Pull_Requests pull_requests;
+	bool pull_requests_loaded;
 } App_State;
 
-App_State app_state = {0};
+global_variable App_State app_state;
 global_variable _Alignas(max_align_t) u8 pr_storage[PR_STORAGE_CAPACITY];
 
-internal void on_counter_button_click(GtkWidget *button, void *user_data) {
-    GtkLabel *label = GTK_LABEL(user_data);
+internal GtkWidget *pull_request_list_new(const Pull_Requests *requests) {
+	GtkWidget *list = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
-    app_state.counter += 1;
-    sprintf(app_state.label_buf, "%d", app_state.counter);
-    gtk_label_set_text(label, app_state.label_buf);
+	for (usize i = 0; i < requests->len; i += 1) {
+		Pull_Request_Card_Data data = {
+			.number = requests->number[i],
+			.title = requests->title[i],
+			.description = requests->body[i],
+			.author = requests->author[i],
+			.url = requests->url[i],
+		};
+
+		gtk_box_append(GTK_BOX(list), pull_request_card_new(data));
+	}
+
+	return list;
 }
 
-internal void on_activate(GtkApplication *app) {
-    char label_buf[1024] = "0";
+internal void on_activate(GtkApplication *app, void *user_data) {
+	App_State *state = user_data;
 
-    GtkWidget *window = gtk_application_window_new(app);
-    GtkWidget *label  = gtk_label_new(label_buf);
-    GtkWidget *button = gtk_button_new_with_label("Increment");
+	GtkWidget *window = adw_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "MRView");
+	gtk_window_set_default_size(GTK_WINDOW(window), 900, 720);
 
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 12);
-    gtk_box_append(GTK_BOX(box), label);
-    gtk_box_append(GTK_BOX(box), button);
+	GtkWidget *toolbar_view = adw_toolbar_view_new();
+	GtkWidget *header_bar = adw_header_bar_new();
+	GtkWidget *window_title = adw_window_title_new("MRView", TEST_REPOSITORY);
+	adw_header_bar_set_title_widget(ADW_HEADER_BAR(header_bar), window_title);
+	adw_toolbar_view_add_top_bar(ADW_TOOLBAR_VIEW(toolbar_view), header_bar);
 
-    g_signal_connect(button, "clicked", G_CALLBACK(on_counter_button_click), label);
+	GtkWidget *scrolled = gtk_scrolled_window_new();
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+	adw_toolbar_view_set_content(ADW_TOOLBAR_VIEW(toolbar_view), scrolled);
 
-    gtk_window_set_title(GTK_WINDOW(window), "Hello");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-    gtk_window_set_child(GTK_WINDOW(window), box);
+	GtkWidget *clamp = adw_clamp_new();
+	adw_clamp_set_maximum_size(ADW_CLAMP(clamp), 760);
+	adw_clamp_set_tightening_threshold(ADW_CLAMP(clamp), 600);
+	gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled), clamp);
 
-    gtk_window_present(GTK_WINDOW(window));
+	GtkWidget *content;
+	if (state->pull_requests_loaded && state->pull_requests.len > 0) {
+		content = pull_request_list_new(&state->pull_requests);
+	} else if (state->pull_requests_loaded) {
+		content = gtk_label_new("No open pull requests.");
+	} else {
+		content = gtk_label_new("Could not load pull requests. Check gh authentication and try again.");
+		gtk_label_set_wrap(GTK_LABEL(content), true);
+	}
+
+	gtk_widget_set_margin_top(content, 18);
+	gtk_widget_set_margin_bottom(content, 18);
+	gtk_widget_set_margin_start(content, 18);
+	gtk_widget_set_margin_end(content, 18);
+	adw_clamp_set_child(ADW_CLAMP(clamp), content);
+
+	adw_application_window_set_content(ADW_APPLICATION_WINDOW(window), toolbar_view);
+	gtk_window_present(GTK_WINDOW(window));
 }
 
 i32 main(i32 argc, char *argv[]) {
-    g_autoptr(AdwApplication) app = adw_application_new("net.mooremoore.practice", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+	app_state.pr_arena = static_arena_init(pr_storage, sizeof pr_storage);
+	app_state.pull_requests_loaded = pull_requests_get(
+		&app_state.pr_arena,
+		&app_state.pull_requests,
+		string_view_from_cstr(TEST_REPOSITORY));
 
-    Static_Arena arena = static_arena_init(pr_storage, sizeof pr_storage);
-    Pull_Requests requests = {0};
-    pull_requests_get(&arena, &requests, string_view_from_cstr("GNOME/libadwaita"));
+	g_autoptr(AdwApplication) app = adw_application_new("net.mooremoore.MRView", G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app, "activate", G_CALLBACK(on_activate), &app_state);
 
-    i32 status = g_application_run(G_APPLICATION(app), argc, argv);
-
-    static_arena_reset(&arena);
-    return status;
+	i32 status = g_application_run(G_APPLICATION(app), argc, argv);
+	static_arena_reset(&app_state.pr_arena);
+	return status;
 }
--- pull_request_card.h
+++ pull_request_card.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <adwaita.h>
+
+#include "base.h"
 #include "string_view.h"
 
 typedef struct {
-	i32         id,
-	String_View title,
-	String_View description,
-	String_View author,
-} Pull_Request_Card_State;
+	i32 number;
+	String_View title;
+	String_View description;
+	String_View author;
+	String_View url;
+} Pull_Request_Card_Data;
 
-GtkWidget *pull_request_card_new(char *title, char *description);
+GtkWidget *pull_request_card_new(Pull_Request_Card_Data data);
